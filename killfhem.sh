498a877c4d9c06d1ab8de6e181477bd14567d82b
hexenmeister <schulzalex@gmx.de>
2013-10-19 21:10:03 +0200

    ggf. mehrere Prozesse beenden

--- killfhem.sh
+++ killfhem.sh
@@ -10,26 +10,35 @@ port=7072
 
 echo Stop fhem
 
-perl fhem.pl $port "shutdown" &
-RETVAL=$?
- 
-# Etwas Zeit geben
-sleep 10;
+RETVAL=0
 
-# FHEM PID suchen
-pid=$(ps -ef | grep -v grep | grep fhem.pl | cut -c10-14);
-if test $pid 
-then
-  echo "killing FHEM. PID: $pid";
-  # Prozess beenden
-  kill $pid;
-  # Etwas warten
-  sleep 3;
-  # Wenn nicht beendet - hart terminieren
-  kill -9 $pid;
-  RETVAL=0
+cnt=$(ps -ef | grep "watchdogloop" | grep -v grep | wc -l)
+if [ "$cnt" -ge "0" ] ; then
+  perl fhem.pl $port "shutdown" &
+  RETVAL=$?
+
+  # Etwas Zeit geben
+  sleep 10;
+  
+  # FHEM PID suchen / beenden
+  #pid=$(ps -ef | grep -v grep | grep fhem.pl | cut -c10-14);
+  #if test $pid 
+  #then
+  
+  # Alle uebriggebliebenen Prozesse beenden
+  for pid in $(ps -ef | grep -v grep | grep fhem.pl | cut -c10-14)
+  do
+    echo "killing FHEM. PID: $pid";
+    # Prozess beenden
+    kill $pid &
+    # Etwas warten
+    sleep 3;
+    # Wenn nicht beendet - hart terminieren
+    kill -9 $pid
+    RETVAL=0
+  done
+  #fi
+
 fi
-
-#exit $RETVAL
 
 return $RETVAL
