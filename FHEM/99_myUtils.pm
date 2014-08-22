0076531b859c308bc78537e973ee0c2842eb1f11
hexenmeister <schulzalex@gmx.de>
2014-08-22 13:53:53 +0200

    Refactoring, Optimizing, Improvement

--- FHEM/99_myUtils.pm
+++ FHEM/99_myUtils.pm
@@ -1,5 +1,5 @@
 ##############################################
-# $Id$
+# $Id: 99_myUtils.pm 0000 2014-08-21 00:00:00Z hexenmeister$
 package main;
 
 use strict;
@@ -1371,8 +1371,7 @@ sub automationHeartbeat() {
 	my $hms = CurrentTime();
 	my $cDate = CurrentDate(); 
 	# es ist egal, an welchen Element man diese Angabe 'anhaengt'... nur ein Container
-	my $lDate = getCtrlData("ctrl_last_automatic_heartbeat");
-	#my $lDate = ReadingsVal(ELEMENT_NAME_CTRL_ROLLADEN_DAY_NIGHT, "ctrl_last_automatic_heartbeat", "");
+	my $lDate = getCtrlData("ctrl_last_automatic_heartbeat_reset");
 	# einmal am Tag zw. 2 und 5 Uhr
 	if($cDate ne $lDate &&  $hms gt "02:00" and $hms lt "05:00") {
 		if(Value(ELEMENT_NAME_CTRL_ANWESENHEIT) ne FAR_AWAY) {
@@ -1382,8 +1381,7 @@ sub automationHeartbeat() {
       #  - ZPumpe in Minimal-Modus
       setValue(ELEMENT_NAME_CTRL_ZIRK_PUMPE, ABSENT);
     }
-    putCtrlData("ctrl_last_automatic_heartbeat", $cDate);
-    #setReading(ELEMENT_NAME_CTRL_ROLLADEN_DAY_NIGHT, "ctrl_last_automatic_heartbeat", $cDate);
+    putCtrlData("ctrl_last_automatic_heartbeat_reset", $cDate);
   }
   
   # TODO: Wenn sich der Wert der Anwesenheit auf Auto geaendert hat (nur bei einer Aenderung!), 
@@ -1406,7 +1404,6 @@ sub setAllAutomatikControlsDefaults() {
 	if(Value(ELEMENT_NAME_CTRL_BESCHATTUNG) eq "???" ||  {ReadingsVal(ELEMENT_NAME_CTRL_BESCHATTUNG,"STATE","???")}) {
 	  setValue(ELEMENT_NAME_CTRL_BESCHATTUNG, AUTOMATIC);
 	}
-	#setHomeAutomaticOn();
 	
 	if(Value(ELEMENT_NAME_CTRL_ANWESENHEIT) eq "???" ||  {ReadingsVal(ELEMENT_NAME_CTRL_ANWESENHEIT,"STATE","???")}) {
     setValue(ELEMENT_NAME_CTRL_ANWESENHEIT, AUTOMATIC);
@@ -1427,18 +1424,28 @@ sub setAllAutomatikControlsDefaults() {
 # aufgerufen werden. Damit wird erreicht, dass alle Uebersteuerungen irgendwann 
 # in einen normalen Zustand uebergehen.
 sub resetAutomatikControls() {
-	setHomeAutomaticOn();
+	setBeschattungAutomaticOn();
 	setHomePresence_Automatic();
 	#setHomePresence_Present();
 	setDayNightRolloAutomaticOn();
 	setValue(ELEMENT_NAME_CTRL_ZIRK_PUMPE, AUTOMATIC);
 }
 
-# Controlblock fuer HomeAutomatic-Schalter
-# Parameter: Neuer Zustand
-sub getHomeAutomaticCtrlBlock($) {
-	my($new_state)=@_;
-	my $ctrl_gl_au = getCtrlData("ctrl_last_global_automatic_change");
+###############################################################################
+# Controlblock: Liefert zu dem Group/Key die Daten der letzten Aufrufe
+# Parameter: Group: Gruppe, die Keys der glichen Gruppe werden zusammengefast.
+#            Key: Neuer Zustand.
+#            Zeitangabe in Sekunden: Fuer diese Zeit wird die Anzahl der Aktionen
+#                   der gleichen Gruppe/Key berechnet. Default = 60 (1 Min).
+# Return:    Array: [Zeit seit der letzten Aktion der gleichen Gruppe,
+#                    Zeit zw. der letzten und der vorletzten Aktion der Gruppe,
+#                    Anzahl der Ereignisse der gleichen Gruppe UND Key,
+#                    Anzahl der Ereignisse der gleichen Gruppe UND Key in letzten N Sekunden]
+###############################################################################
+sub getGenericCtrlBlock($$;$) {
+	my($group, $new_state, $last_time_diff)=@_;
+	if(!defined($last_time_diff)) {$last_time_diff=60;}
+	my $ctrl_gl_au = getCtrlData($group);
 	# Format: [zustand on|off...],[datum/zeit decimal (sec)],[counter],[counter_last_min],[sekunden seit letzter aktion]
 	my $ctrl_cnt=0;
 	my $ctrl_state=undef;
@@ -1479,14 +1486,193 @@ sub getHomeAutomaticCtrlBlock($) {
 	  $ctrl_cnt+=1;	
   }
 	
-	putCtrlData("ctrl_last_global_automatic_change", $new_state.",".$dt_dec.",".$ctrl_cnt.",".$ctrl_cnt_last_min.",".($dt_dec-$ctrl_dt));
+	putCtrlData($group, $new_state.",".$dt_dec.",".$ctrl_cnt.",".$ctrl_cnt_last_min.",".($dt_dec-$ctrl_dt));
 	
 	# Rueckgabe: Sekunden seit der Letzten Abfrage, Sekunden zw. Abfragen davor, GesamtAnzahl gleiche Aktion, Anzahl letzte Minute (gleiche Aktion)
 	return (($dt_dec-$ctrl_dt), $ctrl_sec_since, $ctrl_cnt, $ctrl_cnt_last_min);
 }
 
-# Schatet globale Haus-Automatik ein (setzt ELEMENT_NAME_CTRL_BESCHATTUNG aud AUTOMATIC)
-sub setHomeAutomaticOn() {
+# Controlblock fuer HomeAutomatic-Schalter
+# Parameter: Neuer Zustand
+sub getHomeAutomaticCtrlBlock($) {
+	my($key)=@_;
+	return getGenericCtrlBlock("ctrl_last_global_automatic_change", $key);
+}
+
+###############################################################################
+# Bereitet Texte zur Ansage auf.
+# Ersetzt Umlaute (ä=ae etc.)
+###############################################################################
+sub prepareTextToSpeak($) {
+	my($text) = @_;
+	# TODO
+	return $text;
+}
+
+###############################################################################
+# Bereitet Zahlen zur Ansage auf.
+# Ersetzt Kommas und Punkte durch das Wort 'Komma'.
+###############################################################################
+sub prepareNumToSpeak($) {
+	my($text) = @_;
+	$text =~ s/\./Komma/g;
+  $text =~ s/,/Komma/g;
+	return $text;
+}
+
+# Rundet eine Zahl ohne Nachkommastellen
+sub rundeZahl0($) {
+	my($val)=@_;
+	# Prüfen, ob numerisch
+	if(int($val)>0) {
+		$val = int($val+0.5);
+	}
+	return $val;
+}
+
+# Rundet eine Zahl mit 1-er Nachkommastelle
+sub rundeZahl1($) {
+	my($val)=@_;
+	# Prüfen, ob numerisch
+	if(int($val)>0) {
+		$val = int(10*$val+0.5)/10;
+	}
+	return $val;
+}
+
+# Ausrechnet aus der Zahl der Sekunden Ansage in Stunden und Minuten
+sub sec2DauerSprache($){
+  my ($t) = @_;
+  my $d = int($t/86400); # Tage
+  my $h = int(($t - ($d*86400))/3600); #int($t/3600);
+  my $r = $t - ($h*3600);
+  my $m = int($r/60);
+  my $s = $r - $m*60;
+  my $text="";
+  if($d==1) {
+  	$text.="Ein Tag ";
+    #return sprintf("Ein Tag, %d Stunden und %d Minuten",$d,$h,$m);
+  }
+  if($d>1) {
+  	$text.=$d." Tage ";
+    #return sprintf("%d Tage, %d Stunden und %d Minuten",$d,$h,$m);
+  }
+  if($h==1) {
+  	$text.="eine Stunde ";
+  }
+  if($h>1) {
+  	$text.=$h." Stunden ";
+  }
+  if($m==1) {
+    $text.="eine Minute ";
+  } 
+  if($m>1) {
+    $text.=$m." Minuten ";
+  }
+  if($d==0 && $h==0 && $m==0) {
+  	$text=$s." Sekunden";
+  }
+  return $text;
+}
+
+###############################################################################
+# Sagt Wetterdaten an
+#  Param: Art: Variante der Aussage:
+#         0: Kurzansage, 1: Normal
+###############################################################################
+sub speakWetterDaten(;$) {
+	my($art)=@_;
+	if(!defined($art)){$art=1;}
+	# TODO: Sauber / Abstraktionslayer erstellen
+	my $temp = prepareNumToSpeak(rundeZahl1(ReadingsVal("GSD_1.4","temperature","unbekannt")));
+	my $humi = prepareNumToSpeak(rundeZahl0(ReadingsVal("GSD_1.4","humidity","unbekannt")));
+	if($art==0) {
+    speak("Aussentemperatur ".$temp." Grad. Feuchtigkeit ".$humi." Prozent.");
+  }
+  if($art==1) {
+    speak("Die Aussentemperatur betraegt ".$temp." Grad. Die Luftfeuchtigkeit liegt bei ".$humi." Prozent.");
+  }
+}
+
+###############################################################################
+# Sagt Wettervorhersage an.
+#  Parameter: Tag: Zahl 1-5 (1-heute, 2-morgen,...) Defaul=2
+###############################################################################
+sub speakWetterVorhersage(;$) {
+	my ($day) = @_;
+	if(!defined($day)) {$day=2;}
+	
+	# TODO: Sauber / Abstraktionslayer erstellen
+	my $t1= ReadingsVal("Wetter","fc".$day."_condition",undef);
+	my $t2= ReadingsVal("Wetter","fc".$day."_low_c",undef);
+	my $t3= ReadingsVal("Wetter","fc".$day."_high_c",undef);
+	
+	my $text = "";
+	if($day==1) {
+		$text = "Heute ist es ";
+	}
+	if($day==2) {
+		$text = "Morgen wird es ";
+	}
+	if($day==3) {
+		$text = "Uebermorgen wird es ";
+	}
+	if($day>3) {
+		$text = "Es wird ";
+	}	
+	if(defined($t1) && defined($t2) && defined($t3)) {
+	  $text.=$t1.". ";
+	  $text.="Temperatur von ".$t2." bis ".$t3." Grad.";
+	  if($day==1) {
+	  	# gefuehlte Temperatur
+	  	my $tg= ReadingsVal("Wetter","wind_chill",undef);
+	  	$text.="Gefuehlte Temperatur aktuell ".$tg." Grad.";
+	  	my $tw= ReadingsVal("Wetter","wind_speed",undef);
+	  	$text.="Windgeschwindigkeit ".$tw." Kilometer pro Stunde.";
+	  }
+	} else {
+		$text="Leider keine Vorhersage verfuegbar.";
+	}
+	
+	speak(prepareTextToSpeak($text));
+}
+
+# Methode für Benachrichtigung beim Klingeln an der Haustuer
+sub actHaustuerKlingel() {
+	my($since_last, $sinse_l2, $cnt, $cnt_1min)=getGenericCtrlBlock("ctrl_last_haustuer_klingel", "on", 30);
+	sendMeJabberMessage("Tuerklingel am ".ReadingsTimestamp('KlingelIn','reading',''));
+	
+	my ($sec,$min,$hour,$mday,$month,$year,$wday,$yday,$isdst) = localtime;
+	# nut am Tage
+  if($hour>=6&&$hour<23) {
+  	# 0: ---
+  	# 1: ---
+  	# 2: ? Hundegebell ?
+  	# 3: 
+  	# 4: 
+  	# Wer klingelt da Sturm?
+  	if($cnt_1min==0) {
+			# NOP
+    }
+  	if($cnt_1min==1) {
+			# NOP
+    }
+    if($cnt_1min==2) {
+      speak(":hund1.mp3:",150);
+    }
+    if($cnt_1min==3) {
+      speak(":hund2.mp3:",130);
+    }  
+    if($cnt_1min==4) {
+      speak(":hund7.mp3:",150);
+    }   
+  }
+}
+
+# Methode für den taster
+# Schatet globale Haus-Automatik ein 
+# (setzt ELEMENT_NAME_CTRL_BESCHATTUNG aud AUTOMATIC)
+sub actHomeAutomaticOn() {
 	# Derzeit keine globale Automatik, daher delegieren
 	setBeschattungAutomaticOn();
 	# Tag/Nacht-Steuerung moechte ich hier nicht haben...
@@ -1499,55 +1685,158 @@ sub setHomeAutomaticOn() {
 	my($since_last, $sinse_l2, $cnt, $cnt_1min)=getHomeAutomaticCtrlBlock("on");
 	
 	my ($sec,$min,$hour,$mday,$month,$year,$wday,$yday,$isdst) = localtime;
-	#TODO Alle Ausgaben umbauen / auslagern / sauber implmentieren
-	if($hour>23||$hour<3) {
-		if($cnt_1min<=1) {
+	#$hour=5; # XXX Test
+	#TODO: Alle Ausgaben umbauen / auslagern / sauber implmentieren
+	#TODO: Spezielle Ansage Texte wenn Zustand geaendert ist: 'Nach Hause mommen'
+	
+	# Nachtansage
+	if($hour>=23||$hour<3) {
+		# 0: 
+		# 1: GuteNachtWunsch, ZirkPumpe
+		# 2: Wetter
+		# 3: Wetterprognose für den nächsten Tag (Uhrzeit beanchten: vor/ nach 24:00)
+		# 
+		if($cnt_1min==0) {
+		  # Begrüßung nur, wenn laenger als 10 Min.
+			if($since_last>=600) {
+        speak("Willkommen zurueck!");
+      }
+      # Dauer nur ansagen, wenn laenger als 30 Min.
+      if($since_last>=1800) {
+        speak("Abwesenheitsdauer: ".sec2DauerSprache($since_last));
+      }
+		}
+		if($cnt_1min==1) {
 			# Nicht zu oft wiederholen
       speak("Gute Nacht!");
       
-	    #TODO: ZirkPumpe kurz anwerfen	
-	    # TODO: Sauber / Abstrakt
+	    # ZirkPumpe kurz anwerfen	
+	    # TODO: Sauber / Abstraktionslayer
       fhem("set EG_HA_SA01.Zirkulationspumpe on-for-timer 120");
-    } else {
-    	my $temp = ReadingsVal("GSD_1.4","temperature","unbekannt");
-			my $humi = ReadingsVal("GSD_1.4","humidity","unbekannt");
-      speak("Die Temperatur draussen betraegt ".$temp."Grad. Luftfeuchtigkeit ".$humi." Prozent.");
+    } 
+    if($cnt_1min==2) {
+      speakWetterDaten();
     }
+    if($cnt_1min==3) {
+    	if($hour<=23) {
+    		# fuer morgen
+        speakWetterVorhersage(2);
+      } else {
+      	# fuer jetzt
+        speakWetterVorhersage(1);
+      }
+    }
+    
 	}
 	
-	if($hour>3&&$hour<10) {
-	  if($cnt_1min<=1) {
-			# Nicht zu oft wiederholen
-			my $temp = ReadingsVal("GSD_1.4","temperature","unbekannt");
-			my $humi = ReadingsVal("GSD_1.4","humidity","unbekannt");
-      speak("Guten Morgen! "."Die Temperatur draussen betraegt ".$temp."Grad. Luftfeuchtigkeit ".$humi." Prozent.");
-    } else {
-    	speak("Hallo!");
+	# Morgensansage
+	if($hour>=3&&$hour<10) {
+		# 0: Begrüßung
+		# 1: Begrüßung, Wetterdaten
+		# 2: Wetterprognose
+		# 3: Wiederholen: Wetter und Prognose
+		if($cnt_1min==0) {
+			# Begrüßung nur, wenn laenger als 10 Min.
+			if($since_last>=600) {
+        speak("Willkommen!");
+      }
+      # Dauer nur ansagen, wenn laenger als 30 Min.
+      if($since_last>=1800) {
+        speak("Abwesenheitsdauer: ".sec2DauerSprache($since_last));
+      }
     }
+		if($cnt_1min==1) {
+			# Nicht zu oft wiederholen
+      speak("Guten Morgen!");
+      speakWetterDaten();
+    } 
+    if($cnt_1min==2) {
+      speakWetterVorhersage(1);
+    }
+    if($cnt_1min==3) {
+      speak("Ok, nochmal!.");
+      speakWetterDaten(0);
+      speakWetterVorhersage(1);
+    }    
   }
   
-  if($hour>10) {
-  	if($cnt_1min<=1) {
-			# Nicht zu oft wiederholen
-			my $temp = ReadingsVal("GSD_1.4","temperature","unbekannt");
-			my $humi = ReadingsVal("GSD_1.4","humidity","unbekannt");
-      speak("Hallo! "."Die Temperatur draussen betraegt ".$temp."Grad. Luftfeuchtigkeit ".$humi." Prozent.");
-    } else {
-    	speak("Hallo!");
+  # Tagesansage
+  if($hour>=10&&$hour<23) {
+  	# 0: Begruessung
+  	# 1: Begrüßung, Wetter
+  	# 2: Wetterprognose (nur bis 14 Uhr?), sonst Aktuelles Wetter
+  	# 3: Wiederholen: Wetter und Prognose
+  	if($cnt_1min==0) {
+			# Begrüßung nur, wenn laenger als 10 Min.
+			if($since_last>=600) {
+        speak("Willkommen!");
+      }
+      # Dauer nur ansagen, wenn laenger als 30 Min.
+      if($since_last>=1800) {
+        speak("Abwesenheitsdauer: ".sec2DauerSprache($since_last));
+      }
     }
+  	if($cnt_1min==1) {
+			# Nicht zu oft wiederholen
+      speak("Hallo!");
+      speakWetterDaten();
+    }
+    if($cnt_1min==2) {
+    	if($hour<15) {
+    		# fuer jetzt
+        speakWetterVorhersage(1);
+      } else {
+      	# fuer morgen
+        speakWetterVorhersage(2);
+      }
+    }
+    if($cnt_1min==3) {
+      speak("Ok, nochmal!.");
+      speakWetterDaten(0);
+      if($hour<15) {
+    		# fuer jetzt
+        speakWetterVorhersage(1);
+      } else {
+      	# fuer morgen
+        speakWetterVorhersage(2);
+      }
+    }    
   }
-
+  
+  # Allgemein 
+  if($cnt_1min==4) {
+    # TODO: Meldungen und Systemmeldungen
+  }
+  # 5: Vorstellung
+  if($cnt_1min==6) {
+    speak("Hi! Ich bin Lea. Ich bin für die Ueberwachung und Steuerung zustaendig.");
+    # TODO Versionsangaben
+  }
+  # 6: schweigen
+  # 7/8: Kleiner Scherz ;)
+  if($cnt_1min==8) {
+    speak("Lass doch den Knopf endlich in Ruhe!");
+  }
+  if($cnt_1min==9) {
+    speak("Mit dir spreche ich nicht mehr!");
+  }
+  
 	if($cnt>0) {
-		# wiederholte Aktion (by Aenderung waere cnt=0)
+		# wiederholte Aktion (by Aenderung waere cnt=0).
+		# Die gleich nacheinander folgende Aufrufe sind bereit oben verarbeitet.
+		# Hier koennen davon unabhaengende Sacher erledigt werden.
+		# Z.B. ZirkPumpe etc.
 		# TODO
 	}
 	# TODO
 }
 
-# Schatet globale Haus-Automatik aus (setzt ELEMENT_NAME_CTRL_BESCHATTUNG aud DISABLED)
-sub setHomeAutomaticOff() {
+# Methode für den taster
+# Schatet globale Haus-Automatik aus 
+# (setzt ELEMENT_NAME_CTRL_BESCHATTUNG aud DISABLED)
+sub actHomeAutomaticOff() {
 	# Derzeit keine globale Automatik, daher delegieren
-	setBeschattungAutomaticOff();
+	setBeschattungAutomaticOff(); # ?
 	
   # Hier (Sprach)Meldungen:
 	# Konzept: ein "Knopf-Bedienung": 
@@ -1555,7 +1844,43 @@ sub setHomeAutomaticOff() {
 	#    Wenn Zustand unverändert: Tageszeitabhängige Meldungen
 	#    Auswertung: Wann war dieser Knopf zuletzt gedrueckt? Wie oft?
 	my($since_last, $sinse_l2, $cnt, $cnt_1min)=getHomeAutomaticCtrlBlock("off");
-  # TODO
+  
+  my ($sec,$min,$hour,$mday,$month,$year,$wday,$yday,$isdst) = localtime;
+  
+  # Nachtansage
+	if($hour>=23||$hour<3) {
+	  if($cnt_1min==0) {
+	  # Begrüßung nur, wenn laenger als 10 Min.
+      if($since_last>=300) {
+    	  speakWetterDaten(0);
+        speak("Viel Spass. Bis spaeter!");
+      }
+	  }
+	}
+	
+	# Morgensansage
+	if($hour>=3&&$hour<10) {
+		if($cnt_1min==0) {
+	  # Begrüßung nur, wenn laenger als 10 Min.
+      if($since_last>=300) {
+    	  speakWetterDaten();
+        speak("angenhmen Tag!");
+      }
+	  }
+	}
+	
+	# Tagesansage
+  if($hour>=10&&$hour<23) {
+  	if($cnt_1min==0) {
+	  # Begrüßung nur, wenn laenger als 10 Min.
+      if($since_last>=300) {
+    	  speakWetterDaten();
+        speak("Bis spaeter!");
+      }
+	  }
+  }
+	
+  # TODO: geoeffnete Fenster melden
 }
 
 # Schatet Beschattung-Automatik ein (setzt ELEMENT_NAME_CTRL_BESCHATTUNG aud AUTOMATIC)
