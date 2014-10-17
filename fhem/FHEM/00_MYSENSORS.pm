569b02e9ac9dc66df8cc6cfc79ae6bc806e389ce
ntruchsess <norbert.truchsess@t-online.de>
2014-10-17 07:18:31 +0200

    MYSENSORS: initial commit

---
+++ fhem/FHEM/00_MYSENSORS.pm
@@ -0,0 +1,350 @@
+##############################################
+#
+# fhem driver for MySensors serial or network gateway (see http://mysensors.org)
+#
+# Copyright (C) 2014 Norbert Truchsess
+#
+#     This file is part of fhem.
+#
+#     Fhem is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 2 of the License, or
+#     (at your option) any later version.
+#
+#     Fhem is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with fhem.  If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id$
+#
+##############################################
+
+my %sets = (
+  "connect" => "",
+  "disconnect" => "",
+);
+
+my %gets = (
+  "version"   => ""
+);
+
+my @clients = qw(
+  MYSENSORS_NODE
+);
+
+sub MYSENSORS_Initialize($) {
+
+  my $hash = shift @_;
+
+  require "$main::attr{global}{modpath}/FHEM/DevIo.pm";
+
+  # Provider
+  $hash->{Clients} = join (':',@clients);
+  $hash->{ReadyFn} = "MYSENSORS::Ready";
+  $hash->{ReadFn}  = "MYSENSORS::Read";
+
+  # Consumer
+  $hash->{DefFn}    = "MYSENSORS::Define";
+  $hash->{UndefFn}  = "MYSENSORS::Undef";
+  $hash->{SetFn}    = "MYSENSORS::Set";
+  $hash->{NotifyFn} = "MYSENSORS::Notify";
+
+  $hash->{AttrList} = "keep-alive";
+}
+
+package MYSENSORS;
+
+use Exporter ('import');
+@EXPORT = ();
+@EXPORT_OK = qw(sendMessage);
+%EXPORT_TAGS = (all => [@EXPORT_OK]);
+
+use strict;
+use warnings;
+
+use GPUtils qw(:all);
+
+use Device::MySensors::Constants qw(:all);
+use Device::MySensors::Message qw(:all);
+
+BEGIN {GP_Import(qw(
+  gettimeofday
+  readingsSingleUpdate
+  DevIo_OpenDev
+  DevIo_SimpleWrite
+  DevIo_SimpleRead
+  DevIo_CloseDev
+  RemoveInternalTimer
+  InternalTimer
+  AttrVal
+  Log3
+  ))};
+
+sub Define($$) {
+  my ( $hash, $def ) = @_;
+
+  $hash->{NOTIFYDEV} = "global";
+
+  if ($main::init_done) {
+    return Start($hash);
+  } else {
+    return undef;
+  }
+}
+
+sub Undef($) {
+  Stop(shift);
+}
+
+sub Set($@) {
+  my ($hash, @a) = @_;
+  return "Need at least one parameters" if(@a < 2);
+  return "Unknown argument $a[1], choose one of " . join(" ", sort keys %sets)
+    if(!defined($sets{$a[1]}));
+  my $command = $a[1];
+  my $value = $a[2];
+
+  COMMAND_HANDLER: {
+    $command eq "connect" and do {
+      Start($hash);
+      last;
+    };
+    $command eq "disconnect" and do {
+      Stop($hash);
+      last;
+    };
+  };
+}
+
+sub Notify($$) {
+  my ($hash,$dev) = @_;
+  if( grep(m/^(INITIALIZED|REREADCFG)$/, @{$dev->{CHANGED}}) ) {
+    Start($hash);
+  } elsif( grep(m/^SAVE$/, @{$dev->{CHANGED}}) ) {
+  }
+}
+
+sub Attr($$$$) {
+  my ($command,$name,$attribute,$value) = @_;
+
+  my $hash = $main::defs{$name};
+  ATTRIBUTE_HANDLER: {
+    $attribute eq "keep-alive" and do {
+      if ($command eq "set") {
+        $hash->{timeout} = $value;
+      } else {
+        $hash->{timeout} = 60;
+      }
+      if ($main::init_done) {
+        Timer($hash);
+      };
+      last;
+    };
+  };
+}
+
+sub Start($) {
+  my $hash = shift;
+  my ($dev) = split("[ \t]+", $hash->{DEF});
+  $hash->{DeviceName} = $dev;
+  DevIo_CloseDev($hash);
+  return DevIo_OpenDev($hash, 0, "MYSENSORS::Init");
+}
+
+sub Stop($) {
+  my $hash = shift;
+  send_disconnect($hash);
+  DevIo_CloseDev($hash);
+  RemoveInternalTimer($hash);
+  readingsSingleUpdate($hash,"connection","disconnected",1);
+}
+
+sub Ready($) {
+  my $hash = shift;
+  return DevIo_OpenDev($hash, 1, "MYSENSORS::Init") if($hash->{STATE} eq "disconnected");
+}
+
+sub Init($) {
+  my $hash = shift;
+  readingsSingleUpdate($hash,"connection","connected",1);
+  Timer($hash);
+  return undef;
+}
+
+sub Timer($) {
+  my $hash = shift;
+  RemoveInternalTimer($hash);
+#  InternalTimer(gettimeofday()+$hash->{timeout}, "MYSENSORS::Timer", $hash, 0);
+}
+
+sub Read {
+  my ($hash) = @_;
+  my $name = $hash->{NAME};
+
+  my $buf = DevIo_SimpleRead($hash);
+  return "" if(!defined($buf));
+
+  my $data = $hash->{PARTIAL};
+  Log3 ($name, 5, "MYSENSORS/RAW: $data/$buf");
+  $data .= $buf;
+
+  if($data =~ m/\n/) {
+    my $txt;
+    ($txt,$data) = split("\n", $data, 2);
+    $txt =~ s/\r//;
+    my $msg = parseMsg($txt);
+    Log3 ($name,5,"MYSENSORS Read: ".dumpMsg($msg));
+    
+#  my $msg = { radioId => $fields[0],
+#                 childId => $fields[1],
+#                 cmd     => $fields[2],
+#                 ack     => 0,
+##                 ack     => $fields[3],    # ack is not (yet) passed with message
+#                 subType => $fields[3],
+#                 payload => $fields[4] };
+
+    my $type = $msg->{cmd};
+    MESSAGE_TYPE: {
+      $type == C_PRESENTATION and do {
+        onPresentationMsg($hash,$msg);
+        last;
+      };
+      $type == C_SET and do {
+        onSetMsg($hash,$msg);
+        last;
+      };
+      $type == C_REQ and do {
+        onRequestMsg($hash,$msg);
+        last;
+      };
+      $type == C_INTERNAL and do {
+        onInternalMsg($hash,$msg);
+        last;
+      };
+      $type == C_STREAM and do {
+        onStreamMsg($hash,$msg);
+        last;
+      };
+    }
+  }
+  $hash->{PARTIAL} = $data;
+  return undef;
+};
+
+sub onPresentationMsg($$) {
+  my ($hash,$msg) = @_;
+  my $client = matchClient($hash,$msg);
+  
+  my $sensorType = $msg->{subType};
+  sensorTypesToStr($sensorType) =~ /^S_(.+)$/;
+  my $sensorTypeStr = $1;
+  if ($client) {
+    if ($client->{sensorType} != $sensorType) {
+      if (AttrVal($hash->{NAME},"autocreate","")) {
+        CommandModify(undef,"$client->{NAME} MYSENSORS_NODE $sensorTypeStr $msg->{radioId} $msg->{childId}");
+      } else {
+        Log3($hash->{NAME},3,"MYSENSORS: ignoring presentation-msg different type $sensorType for $client->{NAME} radioId $msg->{radioId}, childId $msg->{childId}, type $client->{sensorType}");
+        readingsSingleUpdate($client,"error","msg different sensorType $sensorType",1);
+      }
+    }
+  } else {
+    if (AttrVal($hash->{NAME},"autocreate","")) {
+      CommandDefine(undef,"MySensor_$sensorTypeStr\_$msg->{radioId}_$msg->{childId} MYSENSORS_NODE $sensorTypeStr $msg->{radioId} $msg->{childId}");
+    } else {
+      Log3($hash->{NAME},3,"MYSENSORS: ignoring presentation-msg from unknown radioId $msg->{radioId}, childId $msg->{childId}, sensorType $sensorType");
+    }
+  }
+};
+
+sub onSetMsg($$) {
+  my ($hash,$msg) = @_;
+  if (my $client = matchClient($hash,$msg)) {
+    MYSENSORS::NODE::onSetMessage($client,$msg);
+  } else {
+    Log3($hash->{NAME},3,"MYSENSORS: ignoring set-msg from unknown radioId $msg->{radioId}, childId $msg->{childId} for ".variableTypeToStr($msg->{subType}));
+  }
+};
+
+sub onRequestMsg($$) {
+  my ($hash,$msg) = @_;
+  if (my $client = matchClient($hash,$msg)) {
+    MYSENSORS::NODE::onRequestMessage($client,$msg);
+  } else {
+    Log3($hash->{NAME},3,"MYSENSORS: ignoring req-msg from unknown radioId $msg->{radioId}, childId $msg->{childId} for ".variableTypeToStr($msg->{subType}));
+  }
+};
+
+sub onInternalMsg($$) {
+  my ($hash,$msg) = @_;
+  readingsSingleUpdate($hash,internalMessageTypesToStr($msg->{subType}),$msg->{payload},1);
+};
+
+sub onStreamMsg($$) {
+  my ($hash,$msg) = @_;
+};
+
+sub sendMessage($$) {
+  my ($hash,$msg) = @_;
+};
+
+sub matchClient($$) {
+  my ($hash,$msg) = @_;
+  my $radioId = $msg->{radioId};
+  my $childId = $msg->{childId};
+  my $found;
+  GP_ForallClients($hash,sub {
+    return if $found;
+    my $client = shift;
+    if ($client->{radioId} == $radioId and $client->{childId} == $childId) {
+      $found = $client;
+    }
+  });
+  return $found;
+}
+
+1;
+
+=pod
+=begin html
+
+<a name="MYSENSORS"></a>
+<h3>MYSENSORS</h3>
+<ul>
+  <p>connects fhem to <a href="http://MYSENSORS.org">MYSENSORS</a>.</p>
+  <p>A single MYSENSORS device can serve multiple <a href="#MYSENSORS_DEVICE">MYSENSORS_DEVICE</a> and <a href="#MYSENSORS_BRIDGE">MYSENSORS_BRIDGE</a> clients.<br/>
+     Each <a href="#MYSENSORS_DEVICE">MYSENSORS_DEVICE</a> acts as a bridge in between an fhem-device and MYSENSORS.<br/>
+     Note: this module is based on module <a href="https://metacpan.org/pod/distribution/Net-MYSENSORS/lib/Net/MYSENSORS.pod">Net::MYSENSORS</a>.</p>
+  <a name="MYSENSORSdefine"></a>
+  <p><b>Define</b></p>
+  <ul>
+    <p><code>define &lt;name&gt; MYSENSORS &lt;ip:port&gt;</code></p>
+    <p>Specifies the MYSENSORS device.</p>
+  </ul>
+  <a name="MYSENSORSset"></a>
+  <p><b>Set</b></p>
+  <ul>
+    <li>
+      <p><code>set &lt;name&gt; connect</code><br/>
+         (re-)connects the MYSENSORS-device to the MYSENSORS-broker</p>
+    </li>
+    <li>
+      <p><code>set &lt;name&gt; disconnect</code><br/>
+         disconnects the MYSENSORS-device from the MYSENSORS-broker</p>
+    </li>
+  </ul>
+  <a name="MYSENSORSattr"></a>
+  <p><b>Attributes</b></p>
+  <ul>
+    <li>
+      <p>keep-alive<br/>
+         sets the keep-alive time (in seconds).</p>
+    </li>
+  </ul>
+</ul>
+
+=end html
+=cut
---
+++ fhem/FHEM/10_MYSENSORS_NODE.pm
@@ -0,0 +1,229 @@
+##############################################
+#
+# fhem bridge to MySensors (see http://mysensors.org)
+#
+# Copyright (C) 2014 Norbert Truchsess
+#
+#     This file is part of fhem.
+#
+#     Fhem is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 2 of the License, or
+#     (at your option) any later version.
+#
+#     Fhem is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with fhem.  If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id$
+#
+##############################################
+
+use strict;
+use warnings;
+
+my %gets = (
+  "version"   => "",
+);
+
+sub MYSENSORS_NODE_Initialize($) {
+
+  my $hash = shift @_;
+
+  # Consumer
+  $hash->{DefFn}    = "MYSENSORS::NODE::Define";
+  $hash->{UndefFn}  = "MYSENSORS::NODE::UnDefine";
+  $hash->{SetFn}    = "MYSENSORS::NODE::Set";
+  $hash->{AttrFn}   = "MYSENSORS::NODE::Attr";
+  
+  $hash->{AttrList} =
+    "IODev ".
+    "setVar ".
+    "set_.* ".
+    $main::readingFnAttributes;
+
+  main::LoadModule("MYSENSORS");
+}
+
+package MYSENSORS::NODE;
+
+use strict;
+use warnings;
+use GPUtils qw(:all);
+
+use Device::MySensors::Constants qw(:all);
+use Device::MySensors::Message qw(:all);
+
+our %sets = (
+  'S_DOOR'  => {},
+  'S_MOTION'  => {},
+  'S_SMOKE'  => {},
+  'S_LIGHT'  => {'V_LIGHT' => ['0','1']},
+  'S_DIMMER'  => {},
+  'S_COVER'  => {},
+  'S_TEMP'  => {},
+  'S_HUM'  => {},
+  'S_BARO'  => {},
+  'S_WIND' => {},
+  'S_RAIN'  => {},
+  'S_UV'  => {},
+  'S_WEIGHT'  => {},
+  'S_POWER'  => {},
+  'S_HEATER'  => {},
+  'S_DISTANCE'  => {},
+  'S_LIGHT_LEVEL'  => {},
+  'S_ARDUINO_NODE' => {},
+  'S_ARDUINO_REPEATER_NODE' => {},
+  'S_LOCK' => {},
+  'S_IR' => {},
+  'S_WATER' => {},
+  'S_AIR_QUALITY' => {},
+);
+
+BEGIN {
+  MYSENSORS->import(qw(:all));
+
+  GP_Import(qw(
+    CommandDeleteReading
+    CommandAttr
+    readingsSingleUpdate
+    AssignIoPort
+    Log3
+  ))
+};
+
+sub Define($$) {
+  my ( $hash, $def ) = @_;
+  my ($name, $type, $sensorType, $radioId, $childId) = split("[ \t]+", $def);
+  return "requires 3 parameters" unless (defined $childId and $childId ne "");
+  return "unknown sensor type $sensorType, must be one of ".join(" ",map { $_ =~ /^S_(.+)$/; $1 } (sensorTypes)) unless grep { $_ eq "S_$sensorType"} (sensorTypes);
+  $hash->{sensorType} = sensorTypeToIdx("S_$sensorType");
+  $hash->{radioId} = $radioId;
+  $hash->{childId} = $childId;
+  $hash->{sets} = {};
+  AssignIoPort($hash);
+};
+
+sub UnDefine($) {
+  my ($hash) = @_;
+}
+
+sub Set($$$@) {
+  my ($hash,$name,$command,@values) = @_;
+  return "Need at least one parameters" unless defined $command;
+  return "Unknown argument $command, choose one of " . join(" ", map {$hash->{sets}->{$_} eq "" ? $_ : "$_:".$hash->{sets}->{$_}} sort keys %{$hash->{sets}})
+    if(!defined($hash->{sets}->{$command}));
+  if (@values) {
+    my $value = join " ",@values;
+    readingsSingleUpdate($hash,$command,$value,1);
+  } else {
+    readingsSingleUpdate($hash,"state",$command,1);
+  }
+  return undef;
+}
+
+sub Attr($$$$) {
+  my ($command,$name,$attribute,$value) = @_;
+
+  my $hash = $main::defs{$name};
+  ATTRIBUTE_HANDLER: {
+    $attribute eq "setVar" and do {
+      if ($command eq "set") {
+        foreach my $set (split ("[, \t]+",$value)) {
+          $hash->{sets}->{$set}="";
+        }
+      } else {
+        foreach my $set (split "[, \t]",AttrVal($name,$attribute,"")) {
+          delete $hash->{sets}->{$set};
+        }
+      }
+      last;
+    };
+    $attribute =~ /^set_(.+)/ and do {
+      if ($command eq "set") {
+        $hash->{sets}->{$1}=join(",",split ("[, \t]+",$value));
+      } else {
+        CommandDeleteReading(undef,"$hash->{NAME} $1");
+        delete $hash->{sets}->{$1};
+      }
+      last;
+    };
+  }
+}
+
+sub onSetMessage($$) {
+  my ($hash,$msg) = @_;
+  variableTypesToStr($msg->{subType}) =~ /^V_(.+)$/;
+  readingsSingleUpdate($hash,$1,$msg->{payload},1);
+}
+
+sub onRequestMessage($$) {
+  my ($hash,$msg) = @_;
+  variableTypesToStr($msg->{subType}) =~ /^V_(.+)$/;
+  sendMessage($hash,createMsg(
+    radioId => $hash->{radioId},
+    childId => $hash->{childId},
+    cmd => C_SET, 
+    subType => $msg->{subType},
+    payload => ReadingsVal($hash->{NAME},$1,""),
+  ));
+}
+
+1;
+
+=pod
+=begin html
+
+<a name="MYSENSORS_NODE"></a>
+<h3>MYSENSORS_NODE</h3>
+<ul>
+  <p>acts as a fhem-device that is mapped to <a href="http://mqtt.org/">mqtt</a>-topics.</p>
+  <p>requires a <a href="#MQTT">MQTT</a>-device as IODev<br/>
+     Note: this module is based on module <a href="https://metacpan.org/pod/distribution/Net-MQTT/lib/Net/MQTT.pod">Net::MQTT</a>.</p>
+  <a name="MYSENSORS_NODEdefine"></a>
+  <p><b>Define</b></p>
+  <ul>
+    <p><code>define &lt;name&gt; MYSENSORS_NODE</code><br/>
+       Specifies the MQTT device.</p>
+  </ul>
+  <a name="MYSENSORS_NODEset"></a>
+  <p><b>Set</b></p>
+  <ul>
+    <li>
+      <p><code>set &lt;name&gt; &lt;command&gt;</code><br/>
+         sets reading 'state' and publishes the command to topic configured via attr publishSet</p>
+    </li>
+    <li>
+      <p><code>set &lt;name&gt; &lt;h;reading&gt; &lt;value&gt;</code><br/>
+         sets reading &lt;h;reading&gt; and publishes the command to topic configured via attr publishSet_&lt;h;reading&gt;</p>
+    </li>
+  </ul>
+  <a name="MYSENSORS_NODEattr"></a>
+  <p><b>Attributes</b></p>
+  <ul>
+    <li>
+      <p><code>attr &lt;name&gt; publishSet [&lt;commands&gt;] &lt;topic&gt;</code><br/>
+         configures set commands that may be used to both set reading 'state' and publish to configured topic</p>
+    </li>
+    <li>
+      <p><code>attr &lt;name&gt; publishSet_&lt;reading&gt; [&lt;values&gt;] &lt;topic&gt;</code><br/>
+         configures reading that may be used to both set 'reading' (to optionally configured values) and publish to configured topic</p>
+    </li>
+    <li>
+      <p><code>attr &lt;name&gt; autoSubscribeReadings &lt;topic&gt;</code><br/>
+         specify a mqtt-topic pattern with wildcard (e.c. 'myhouse/kitchen/+') and MYSENSORS_NODE automagically creates readings based on the wildcard-match<br/>
+         e.g a message received with topic 'myhouse/kitchen/temperature' would create and update a reading 'temperature'</p>
+    </li>
+    <li>
+      <p><code>attr &lt;name&gt; subscribeReading_&lt;reading&gt; &lt;topic&gt;</code><br/>
+         mapps a reading to a specific topic. The reading is updated whenever a message to the configured topic arrives</p>
+    </li>
+  </ul>
+</ul>
+
+=end html
+=cut
