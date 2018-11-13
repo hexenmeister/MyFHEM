798c57d49d0ae496edcab5c55afe054dfc2ec138
hexenmeister <schulzalex@gmx.de>
2018-11-13 19:35:52 +0100

    halloween zeiten

--- FHEM/99_myCtrlVoice.pm
+++ FHEM/99_myCtrlVoice.pm
@@ -716,7 +716,7 @@ sub voiceHalloween($) {
   my ($sec,$min,$hour,$mday,$month,$year,$wday,$yday,$isdst) = localtime;
   # nur am Halloween-Abend ab 16:00
   #if($month==9 && $mday==31 && $hour>=16 && $hour<23) {
-  if($month==9 && $mday==31 && $hour>=17 && $hour<20) {
+  if($month==9 && $mday==31 && $hour>=11 && $hour<22) {
     
     # Schrei (Tuerklingel)
     if($mode eq "0") {
